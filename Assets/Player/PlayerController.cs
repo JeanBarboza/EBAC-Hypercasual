bd2a748799da4267e2416b8a5d873693321e170a
JeanBarboza <jeanbarboza.2k@gmail.com>
2021-11-01 18:06:00 -0300

    Add Power Up Speed Up

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Barboza.Core.Singleton;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Singleton<PlayerController>
 {
     //Publics
     [Header("Lerp")]
@@ -19,8 +20,14 @@ public class PlayerController : MonoBehaviour
     //Privates
     private bool _canRun;
     private Vector3 _pos;
+    private float _currentSpeed;
 
-    
+    private void Start()
+    {
+        ResetSpeed();
+    }
+
+
     void Update()
     {
         if (!_canRun) return;
@@ -30,7 +37,7 @@ public class PlayerController : MonoBehaviour
         _pos.z = transform.position.z;
 
         transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
-        transform.Translate(transform.forward * speed * Time.deltaTime);
+        transform.Translate(transform.forward * _currentSpeed * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -60,4 +67,16 @@ public class PlayerController : MonoBehaviour
     {
         _canRun = true;
     }
+
+    #region PowerUpSpeedUp
+    public void PowerUpSpeedUp(float f)
+    {
+        _currentSpeed = f;
+    }
+
+    public void ResetSpeed()
+    {
+        _currentSpeed = speed;
+    }
+    #endregion
 }
