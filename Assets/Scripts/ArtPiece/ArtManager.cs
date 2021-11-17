f1d602936010cb1097ef8862c68488f94cd559d6
JeanBarboza <jeanbarboza.2k@gmail.com>
2021-11-17 16:07:07 -0300

    Create levels/colors managers

---
+++ Assets/Scripts/ArtPiece/ArtManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Barboza.Core.Singleton;
+
+public class ArtManager : Singleton<ArtManager>
+{
+    public enum ArtType
+    {
+        Type_01,
+        Type_02,
+        Type_03,
+        Type_04
+    }
+
+    public List<ArtSetup> artSetups;
+
+
+    public ArtSetup GetSetupByType(ArtType artType)
+    {
+        return artSetups.Find(i => i.artType == artType);
+    }
+}
+
+[System.Serializable]
+ public class ArtSetup
+{
+    public ArtManager.ArtType artType;
+    public GameObject gameObject;
+}
